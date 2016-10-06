Fixed prediction formula

auxiliar/tagsimilarity.py
@@ -2,8 +2,20 @@ import sqlite3
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 np.set_printoptions(threshold=np.inf)
+import random
+import opening_feat as of
+
+user_features = of.load_features('../users_tfidf_profile.bin')
+movie_features = of.load_features('../movies_tfidf_profile.bin')
+
+print user_features[121]
+# print movie_features[47]
+
+exit()
 
 conn = sqlite3.connect('../database.db')
+
+# I deleted all the movie tags that were applied less than 50 times
 
 # select users and contrast user-item profiles
 
@@ -62,16 +74,18 @@ def get_user_profile(user):
     sql_tags = "SELECT t.tag, COUNT(*) " \
                "FROM movielens_tag t " \
                "JOIN movielens_rating r ON r.userid = t.userid AND r.movielensid = t.movielensid " \
-               "WHERE t.userid = ? AND r.rating > 3 " \
+               "WHERE t.userid = ? AND r.rating > 4 " \
                "GROUP BY t.tag"
 
     c = conn.cursor()
     c.execute(sql_tags, (user,))
     tags = c.fetchall()
+    training_set = random.sample(tags, int(len(tags) * 0.8))
+    # print tags
 
     user_profile_tfidf = np.zeros(len(all_tags), dtype=np.float)
 
-    for tag in tags:
+    for tag in training_set:
         tf = float(tag[1])
         idf = float(np.log(TOTAL_USERS / getUsersByTag(conn, tag[0])))
         tfidf = float(tf * idf)
@@ -84,7 +98,7 @@ def get_movie_profile(movie):
 
     sql_tags = "SELECT tag, COUNT(*) FROM movielens_tag t " \
                "JOIN movielens_rating r ON r.userid = t.userid AND r.movielensid = t.movielensid " \
-               "WHERE t.movielensid = ? AND r.rating > 3 " \
+               "WHERE t.movielensid = ? AND r.rating > 4 " \
                "GROUP BY tag"
 
     c = conn.cursor()
@@ -104,7 +118,11 @@ def get_movie_profile(movie):
 for user_profile in user_profiles:
 
     user_tfidf = get_user_profile(user_profile[0])
+    if np.sum(user_tfidf) == 0:
+        continue
     userid = user_profile[0]
+
+    print userid
 
     user_recommendations = []
 
@@ -120,14 +138,17 @@ for user_profile in user_profiles:
         sim = cosine_similarity([movie_tfidf], [user_tfidf])
         user_recommendations.append((movie[0], sim))
 
-        print sim
+        # print sim
+    break
 
-
-    users[userid] = user_recommendations
-    exit()
+    # users[userid] = user_recommendations
+    # exit()
 
     # print users
     # exit()
 
-kMostSimilar = sorted(users[userid], key=lambda tup: tup[1], reverse=True)[:30]
+kMostSimilar = sorted(user_recommendations, key=lambda tup: tup[1], reverse=True)[:30]
 print kMostSimilar
+
+# kMostSimilar = sorted(users[userid], key=lambda tup: tup[1], reverse=True)[:30]
+# print kMostSimilar

user_profile_analysis.py
@@ -6,11 +6,15 @@ from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn import svm
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
+import warnings
+warnings.filterwarnings('error')
 
 conn = sqlite3.connect('database.db')
 
 LOW_LEVEL_FEATURES, DEEP_FEATURES, HYBRID_FEATURES = utils.extract_features()
-features = DEEP_FEATURES
+USER_TFIDF_FEATURES, MOVIE_TFIDF_FEATURES = utils.extract_tfidf_features()
+features = DEEP_FEATURES.copy()
 
 user = 96
 user = 156
@@ -25,51 +29,65 @@ sql = "SELECT t.id, r.rating, m.movielensid, m.title " \
       "WHERE t.best_file = 1 " \
       "AND r.userid = ? " \
       # "AND CAST(ms.imdbvotes AS NUMERIC) > 100 "
-c = conn.cursor()
-c.execute(sql, (user,))
 
-all_movies = c.fetchall()
+users = utils.selectRandomUsers(conn)
+total_accuracy = 0
+dataset_size = len(users)
 
-try:
-    full_training_set = random.sample(all_movies, int(len(all_movies) * 0.8))
+for user in users:
 
-    elite_training_set = []
-    for item in full_training_set:
-        if item[1] >= 4:
-            elite_training_set.append(item)
+    c = conn.cursor()
+    c.execute(sql, (user[0],))
 
-    full_test_set = [x for x in all_movies if x not in full_training_set]
-    elite_test_set = []
-    for item in full_test_set:
-        if item[1] >= 4:
-            elite_test_set.append(item)
-except:
-    print "Error"
-    raise
+    all_movies = c.fetchall()
 
-userInstances, userValues = utils.getUserInstances(full_training_set, features)
-train_values = [1 if x > 4 else 0 for x in userValues]
-test_instances, test_values = utils.getUserInstances(full_test_set, features)
-test_real_values = [1 if x > 4 else 0 for x in test_values]
-# gnb = GaussianNB()
-# model = gnb.fit(userInstances, train_values)
-clf = svm.SVC(kernel='rbf', C=0.1)
-clf.fit(userInstances, train_values)
-y_pred = clf.predict(test_instances)
-# print y_pred
-print "SVM"
-print("Number of mislabeled points out of a total %d points : %d, inaccuracy: %d"
-      % (np.array(test_instances).shape[0],(test_real_values != y_pred).sum(),
-         ((test_real_values != y_pred).sum()*100)/np.array(test_instances).shape[0])+"%" )
+    try:
+        full_training_set = random.sample(all_movies, int(len(all_movies) * 0.7))
+        full_test_set = [x for x in all_movies if x not in full_training_set]
+    except:
+        print "Error"
+        raise
+
+    userInstances, userValues = utils.getUserInstances(full_training_set, features)
+
+    # print userInstances
+
+    train_values = [1 if x > 4 else 0 for x in userValues]
+    test_instances, test_values = utils.getUserInstances(full_test_set, features)
+    test_real_values = [1 if x > 4 else 0 for x in test_values]
+
+    # print test_instances
+
+    clf = svm.SVC(kernel='sigmoid', C=100, class_weight={0: 6})
+    try:
+        clf.fit(userInstances, train_values)
+        y_pred = clf.predict(test_instances)
+        # print y_pred
+        # print np.array(test_real_values)
+        # print (np.array(test_real_values) != y_pred)
+        # print (np.array(test_real_values) != y_pred).sum()
+        # print (test_real_values != y_pred).sum()
+        accuracy = 100 - (((test_real_values != y_pred).sum() * 100) / np.array(test_instances).shape[0])
+        total_accuracy += accuracy
+    except:
+        dataset_size -= 1
+        continue
+
+print "Average Accuracy for", dataset_size, "users: ", (total_accuracy/dataset_size), "%"
+    # print y_pred
+    # print "SVM"
+    # print("Number of mislabeled points out of a total %d points : %d, inaccuracy: %d"
+    #       % (np.array(test_instances).shape[0],(test_real_values != y_pred).sum(),
+    #          ((test_real_values != y_pred).sum()*100)/np.array(test_instances).shape[0])+"%" )
 
 
-print "kNN"
-neigh = KNeighborsClassifier(n_neighbors=20, weights='distance')
-neigh.fit(userInstances, train_values)
-y_pred = neigh.predict(test_instances)
-print("Number of mislabeled points out of a total %d points : %d, inaccuracy: %d"
-      % (np.array(test_instances).shape[0],(test_real_values != y_pred).sum(),
-         ((test_real_values != y_pred).sum()*100)/np.array(test_instances).shape[0])+"%" )
+# print "kNN"
+# neigh = KNeighborsClassifier(n_neighbors=20, weights='distance')
+# neigh.fit(userInstances, train_values)
+# y_pred = neigh.predict(test_instances)
+# print("Number of mislabeled points out of a total %d points : %d, inaccuracy: %d"
+#       % (np.array(test_instances).shape[0],(test_real_values != y_pred).sum(),
+#          ((test_real_values != y_pred).sum()*100)/np.array(test_instances).shape[0])+"%" )
 
 # print "Gradient Boosting"
 # clf = GradientBoostingClassifier(n_estimators=1000, learning_rate=0.1, max_depth=1, random_state=0).fit(userInstances, train_values)

utils.py
@@ -54,14 +54,16 @@ def selectRandomUsers(conn):
                  "JOIN movielens_rating r ON r.userId = u.userId " \
                  "JOIN movielens_movie m ON m.movielensid = r.movielensid " \
                  "JOIN trailers t ON t.imdbid = m.imdbidtt " \
-                 "WHERE t.best_file = 1 LIMIT 60 "
+                 "WHERE t.best_file = 1 "\
                  # "GROUP BY r.userId HAVING COUNT(r.movielensId) > 200 " \
                  # "LIMIT 1 "
 
     c = conn.cursor()
     c.execute(queryUsers)
     all_users = c.fetchall()
-    Users = random.sample(all_users, int(len(all_users)*1)) #Users for this iteration
+    Users = all_users
+    # Users = random.sample(all_users, int(len(all_users)*1)) #Users for this iteration
+    # Users = random.sample(all_users, 100)
     # Users = c.fetchall()
 
     return Users
@@ -125,7 +127,7 @@ def getRandomMovieSet(conn, user):
           "WHERE r.userid = ? "
     # "AND CAST(imdbvotes AS NUMERIC) > 200 " \
     # print sql, user
-    limit = 100
+    limit = 200
     c = conn.cursor()
     c.execute(sql, (user,))
     all_movies = c.fetchall()
@@ -196,7 +198,7 @@ def getUserInstances(userMovies, featureVector):
     for movie in userMovies:
         try:
             features = featureVector[movie[0]]
-            all_features.append(features)
+            all_features.append(list(features))
             all_values.append(movie[1])
         except KeyError:
             continue
@@ -204,7 +206,7 @@ def getUserInstances(userMovies, featureVector):
     return all_features, all_values
 
 
-# Return 70% of the user's rated movies
+# Return a percentage of the user's rated movies
 def getUserTrainingTestMovies(conn, user):
     sql = "SELECT t.id, r.rating, m.movielensid, m.title " \
           "FROM trailers t " \
@@ -250,18 +252,19 @@ def convert_array(text):
 
 
 def extract_features():
-    DEEP_FEATURES = load_features('resnet_152_lstm_128.dct')
-    # DEEP_FEATURES = load_features('bof_128.bin')
-    arr = np.array([x[1] for x in DEEP_FEATURES.iteritems()])
-    scaler = preprocessing.StandardScaler().fit(arr)
-    std = scaler.transform(arr)
-    DEEP_FEATURES = {k: v for k, v in it.izip(DEEP_FEATURES.keys(), std)}
 
     LOW_LEVEL_FEATURES = load_features('low_level_dict.bin')
     arr = np.array([x[1] for x in LOW_LEVEL_FEATURES.iteritems()])
     scaler = preprocessing.StandardScaler().fit(arr)
     std = scaler.transform(arr)
     LOW_LEVEL_FEATURES = {k: v for k, v in it.izip(LOW_LEVEL_FEATURES.keys(), std)}
+
+    DEEP_FEATURES = load_features('resnet_152_lstm_128.dct')
+    # DEEP_FEATURES = load_features('bof_128.bin')
+    arr = np.array([x[1] for x in DEEP_FEATURES.iteritems()])
+    scaler = preprocessing.StandardScaler().fit(arr)
+    std = scaler.transform(arr)
+    DEEP_FEATURES = {k: v for k, v in it.izip(DEEP_FEATURES.keys(), std)}
 
     HYBRID_FEATURES = {}
 
@@ -272,3 +275,10 @@ def extract_features():
             continue
 
     return LOW_LEVEL_FEATURES, DEEP_FEATURES, HYBRID_FEATURES
+
+
+def extract_tfidf_features():
+    user_features = load_features('users_tfidf_profile.bin')
+    movie_features = load_features('movies_tfidf_profile.bin')
+
+    return user_features, movie_features